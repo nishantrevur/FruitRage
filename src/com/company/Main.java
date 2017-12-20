c769ff3ee4d7e09c6d3a4db039e738f97028b33a
Nishant Revur <nishant.revur@yahoo.in>
2017-12-20 17:20:56 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         long st = System.currentTimeMillis();
-        FruitRageGame3 game = new FruitRageGame3();
+        FruitRageGame game = new FruitRageGame3();
         game.start();
         long end = System.currentTimeMillis()-st;
         System.out.println("Time taken: "+end);
